499a6f615585e5a28a540611959e536e5df55053
Jason Hutchinson <jhutchinson@wehco.com>
2017-04-14 13:18:20 -0500

    add example

---
+++ examples/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	ms "git.zikes.me/multistatus"
+)
+
+func main() {
+	ws := ms.New()
+
+	for i := 0; i < 10; i++ {
+		w := ws.Add(fmt.Sprintf("Task #%d", i))
+		go func(w *ms.Worker, i int) {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(8000)))
+			if rand.Intn(5) == 1 {
+				w.Fail()
+			} else {
+				w.Done()
+			}
+		}(w, i)
+	}
+
+	ws.Print()
+}
